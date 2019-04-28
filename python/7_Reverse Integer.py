2e336cdb9d6669ed964419a51cbffaab8dab1df8
Judy Liou <37167429+judyliou@users.noreply.github.com>
2019-04-28 15:17:18 +0800

    Update 7_Reverse Integer.py

--- python/7_Reverse Integer.py
+++ python/7_Reverse Integer.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution(object):
     def reverse(self, x):
         """
@@ -11,3 +12,14 @@ class Solution(object):
             x /= 10
         new_x *= flag 
         return new_x if new_x < 2147483648 and new_x >= -2147483648 else 0
+
+    
+# Soluiton 2
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x = int(str(x)[::-1]) if x > 0 else -int(str(-x)[::-1])
+        return x if x < 2147483648 and x >= -2147483648 else 0
