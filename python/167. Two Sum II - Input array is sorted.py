2410e0f6bf65c35b5d32aae9c0fb8c9cd67aaf6b
Judy Liou <37167429+judyliou@users.noreply.github.com>
2019-09-24 10:17:16 -0400

    Add 167. Two Sum II - Input array is sorted.py

---
+++ python/167. Two Sum II - Input array is sorted.py
@@ -0,0 +1,18 @@
+# Time Complexity: O(n)
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0, len(numbers)-1
+        
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum < target:
+                l += 1
+            elif sum > target:
+                r -= 1
+            else:
+                return [l+1, r+1]
