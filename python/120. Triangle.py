ceeff9ceac1de79dc0b5a5a48db45de92d7dd3f7
Judy Liou <37167429+judyliou@users.noreply.github.com>
2019-09-24 21:19:40 -0400

    Add 120. Triangle.py

---
+++ python/120. Triangle.py
@@ -0,0 +1,27 @@
+# O(n) space
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle: return 0
+        f = [0] * (len(triangle) + 1)
+        for row in triangle[::-1]:
+            for i in range(len(row)):
+                f[i] = row[i] + min(f[i], f[i+1])
+        return f[0]
+        
+# Modify the original triangle
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle: return 0
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
+        
