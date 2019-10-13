24ce8909657667312454fdde769fb4cf64c14558
Judy Liou <37167429+judyliou@users.noreply.github.com>
2019-10-13 08:50:03 -0400

    Create 217. Contains Duplicate.py

---
+++ python/217. Contains Duplicate.py
@@ -0,0 +1,28 @@
+# Hash Table
+# Time: O(n), Space: O(n)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        if nums is None:
+            return False
+        else:
+            num_dict = {}
+            for i in nums:
+                if num_dict.get(i) is None:
+                    num_dict[i] = 1
+                else:
+                    return True
+            return False
+            
+# Sorted
+# Time: O(nlogn), Space: O()
+class Solution(object):
+    def containsDuplicate(self, nums):
+        if nums is None:
+            return False
+        else:
+            nums.sort()
+            for i in range(len(nums)-1):
+                if (nums[i] - nums[i+1]) == 0:
+                    return True
+            return False
+            
