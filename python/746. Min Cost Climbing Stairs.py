226ca4bce046ad8991f57dcc0860deced8f73948
Judy Liou <37167429+judyliou@users.noreply.github.com>
2019-12-12 09:40:27 -0500

    Create 746. Min Cost Climbing Stairs.py

---
+++ python/746. Min Cost Climbing Stairs.py
@@ -0,0 +1,11 @@
+def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(cost)
+        if n <= 2:
+            return min(cost)
+        for i in range(2, n):
+            cost[i] = cost[i] + min(cost[i-1], cost[i-2])
+        return min(cost[n-1], cost[n-2])
