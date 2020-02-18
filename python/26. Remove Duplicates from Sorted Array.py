2868fc1704f729fb9cdd8f3535b225f031c7c139
Judy Liou <37167429+judyliou@users.noreply.github.com>
2020-02-18 12:13:27 -0500

    Update 26. Remove Duplicates from Sorted Array.py

--- python/26. Remove Duplicates from Sorted Array.py
+++ python/26. Remove Duplicates from Sorted Array.py
@@ -1,38 +1,33 @@
 # Solution 1
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        if len(nums) == 0:
-            return 0
-        
-        # two pointers
-        i = 0
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
-        return i + 1
+def removeDuplicates(self, nums):
+    if len(nums) == 0:
+        return 0
+    # two pointers
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+    return i + 1
    
+# Solution 2:
+def removeDuplicates(self, nums):
+    if len(nums) == 0:
+        return 0
+    i = 1
+    while i < len(nums):
+        if nums[i] == nums[i-1]:
+            nums.pop(i)
+        else:
+            i += 1
+    return len(nums)
 
-# Solution 2
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        if len(nums) == 0:
-            return 0
-        
-        i = 1
-        while i < len(nums):
-            if nums[i] == nums[i-1]:
-                nums.pop(i)
-            else:
-                i += 1
-        return len(nums)
+# Solution 3: remove from back
+def removeDuplicates(self, nums):
+    if nums == None:
+        return 0
+    for i in range(len(nums)-1, 0, -1):
+        if nums[i] == nums[i-1]:
+            del nums[i]
+    return len(nums)
+                
