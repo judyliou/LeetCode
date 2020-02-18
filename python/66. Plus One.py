2c3b89b57f5aa00438b57cc7db5c3a71538ff7d0
Judy Liou <37167429+judyliou@users.noreply.github.com>
2020-02-18 12:45:23 -0500

    Update 66. Plus One.py

--- python/66. Plus One.py
+++ python/66. Plus One.py
@@ -1,18 +1,24 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        n = len(digits)
-        for i in range(n):
-                last = digits.pop()
-                if last == 9:
-                    continue
-                else:
-                    digits.append(last + 1)
-                    digits.extend([0] * i)
-                    return digits
-        digits = [1]
-        digits.extend([0] * n)
-        return digits
+# Solution 1
+def plusOne(self, digits):
+    n = len(digits)
+    for i in range(n):
+            last = digits.pop()
+            if last == 9:
+                continue
+            else:
+                digits.append(last + 1)
+                digits.extend([0] * i)
+                return digits
+    digits = [1]
+    digits.extend([0] * n)
+    return digits
+
+# Solution 2
+def plusOne(self, digits):    
+    num = 0
+        level = 1
+        for i in range(len(digits)-1, -1, -1):
+            num += digits[i] * level
+            level *= 10
+        num += 1
+        return [i for i in str(num)]
