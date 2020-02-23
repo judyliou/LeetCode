7ae7a3ad2665d9ebcfe48cac89bdfe39916398cc
Judy Liou <37167429+judyliou@users.noreply.github.com>
2020-02-23 11:50:37 -0500

    Update 204. Count Primes.py

--- python/204. Count Primes.py
+++ python/204. Count Primes.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution(object):
     def generate_prime(self, n):
         primes = [1] * (n)
@@ -13,3 +14,18 @@ class Solution(object):
         primes = self.generate_prime(n)
         return sum(primes[2:])
     
+
+# Solution 2
+class Solution(object):
+    def countPrimes(self, n):
+        if n <= 2:
+            return 0
+        
+        arr = [1] * (n+1)
+        arr[0] = arr[1] = 0
+        for i in range(2, int(n ** 0.5) + 1):
+            if arr[i] == 1:  # if is 0, it's multiples must become 0 already as well
+                for j in range(i*i, n+1, i):  # start from i*i because i*(i-1), i*(i-2),etc. have already be checked by (i-1), (i-2)
+                    arr[j] = 0
+        arr[n] = 0
+        return sum(arr)
